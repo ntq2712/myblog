54169f90f843ce374827b6819ef03565e66581ea
quinguyen <qnguyen@nwemon.com>
2025-01-27 23:15:55 +0700

    docker file

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+From mcr.microsoft.com/dotnet/aspnet:9.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /src
+COPY ["blog.csproj", "./"]
+RUN dotnet restore "blog.csproj"
+COPY ..
+RUN dotnet publish -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=build /app/publish .
+ENTRYPOINT ["dotnet", "blog.dll"]
--- obj/Debug/net9.0/blog.AssemblyInfo.cs
+++ obj/Debug/net9.0/blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf3a5cd38622cb2a1b4ef87e87b2303ccc5acb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
