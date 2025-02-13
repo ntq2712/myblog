0711082c09eda0fd1975e7a87ed33e885e4affa7
quinguyen <qnguyen@nwemon.com>
2025-02-13 12:06:06 +0700

    feat(swagger): fix enable swagger

--- Program.cs
+++ Program.cs
@@ -66,12 +66,10 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthentication();
 app.UseAuthorization();
